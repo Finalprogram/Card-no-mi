e1c7aff0729284a0360992dbb1545886f01972dc
Murilo Queiroz Machado <114034676+Finalprogram@users.noreply.github.com>
2025-11-02 18:21:31 -0300

    exlusão rotas de teste

--- src/routes/welcomeRoutes.js
+++ src/routes/welcomeRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const welcomeController = require('../controllers/welcomeController');
 const { isAuthPage } = require('../middleware/auth');
 
-router.get('/step1', welcomeController.showStep1);
+router.get('/step1', isAuthPage, welcomeController.showStep1);
 router.post('/step1', isAuthPage, welcomeController.handleStep1);
-router.get('/step2', welcomeController.showStep2);
+router.get('/step2', isAuthPage, welcomeController.showStep2);
 router.post('/step2', isAuthPage, welcomeController.handleStep2);
 
 module.exports = router;
